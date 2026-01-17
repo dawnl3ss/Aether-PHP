7d6fc9dfbd083bcc20a2b46c70b6b7dc9c04fac2
.dawnless <52249849+dawnl3ss@users.noreply.github.com>
2026-01-17 20:13:27 +0100

    update [2.2.8] : View templating enhanced

--- src/Aether/View/ViewInstance.php
+++ src/Aether/View/ViewInstance.php
@@ -32,22 +32,38 @@ final class ViewInstance implements  ViewInterface {
     /** @var array $_vars */
     private array $_vars;
 
+    /** @var string $_ext */
+    private static $_ext = "php";
+
     public function __construct(string $path, array $vars){
         $this->_path = $path;
         $this->_vars = $vars;
     }
 
+    /**
+     * Render the provided view
+     *
+     * @return void
+     */
     public function _render(){
-        # - We extract and translate data from self::$_vars into php variables
-        extract($this->_vars, EXTR_SKIP | EXTR_REFS);
+        $fullpath = "public/views/" . $this->_path . "." . self::$_ext;
 
-        $fullpath = "public/" . $this->_path . ".php";
+        # - Security check : if extension is not php then we do NOT want any php executed
+        if (self::$_ext !== "php"){
+            echo file_get_contents($fullpath);
+            return;
+        }
+
+        # - We extract and translate data from self::$_vars into php variables
+        \extract($this->_vars, EXTR_SKIP);
 
         if (!file_exists($fullpath))
             die("[View] - Error - Template not found : {$fullpath}");
 
         # - We turn output bufferin on and we include the given view page
+        \ob_start();
         require_once $fullpath;
+        echo \ob_get_clean();
     }
 
 
@@ -58,4 +74,14 @@ final class ViewInstance implements  ViewInterface {
     public static function _make(string $path, array $vars){
         (new self($path, $vars))->_render();
     }
+
+    /**
+     * @return string
+     */
+    public static function _getExtension() : string { return self::$_ext; }
+
+    /**
+     * @param string $_ext
+     */
+    public static function _setExtension(string $_ext){ self::$_ext = $_ext; }
 }
